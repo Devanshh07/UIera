87a69e17ceb0775cae7d6fbc83f32e4d26c71dc8
Devanshh07 <devbaghel457@gmail.com>
2023-10-10 00:02:04 +0530

    UIera

---
+++ app.js
@@ -0,0 +1,8 @@
+
+ // set interval means => our code is rain after given time interval in a contineus 
+// manner execute - timeinterval- then again execute 
+
+  let id = setTimeout(()=>{
+    console.log("codewithdevansh01");
+},2000); //but settimeoutt function is a function that execute the given time interval
+// and one more important think is the time interval is in ms
